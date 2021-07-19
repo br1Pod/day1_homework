33c6c9c2102f209164ef847a2bf3569995d467e5
briPod <33463220+br1Pod@users.noreply.github.com>
2021-07-19 20:00:32 +0100

    second commit

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,4 +1,7 @@
 # print("Stop! /n Who would cross the Bridge of Death must answer me these questions three, /n ere the other side he see",)
+# cannot get the print to print line breaks? Have commented out and will research further.
+
+
 print("Stop!")
 print("Who would cross the Bridge of Death must answer me these questions three,")
 print("ere the other side he see  ")
@@ -9,6 +12,6 @@ colour = input("What is your favourite colour? ")
 
 if colour == "blue":
     print("Right then.., off you go")
-    print("*", name, "crosses the bridge safely, and continues to", quest, "*")
+    print("***", name, "crosses the bridge safely, and continues to", quest, "***")
 else:
     print(name, "is cast into the Gorge of Eternal Peril!")
