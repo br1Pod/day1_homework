ed97518d502e32750ea5883d4bfcea9f6b6edb3f
briPod <33463220+br1Pod@users.noreply.github.com>
2021-07-19 18:38:13 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+# print("Stop! /n Who would cross the Bridge of Death must answer me these questions three, /n ere the other side he see",)
+print("Stop!")
+print("Who would cross the Bridge of Death must answer me these questions three,")
+print("ere the other side he see  ")
+
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+colour = input("What is your favourite colour? ")
+
+if colour == "blue":
+    print("Right then.., off you go")
+    print("*", name, "crosses the bridge safely, and continues to", quest, "*")
+else:
+    print(name, "is cast into the Gorge of Eternal Peril!")
